3d1fe564cd77b656d12264b5124387ff91539f04
Kuba Suder <jakub.suder@gmail.com>
2019-06-12 22:34:18 +0200

    initial commit

---
+++ DarkModeSwitcher/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DarkModeSwitcher
+//
+//  Created by Kuba Suder on 12.06.2019.
+//  Copyright © 2019 Kuba Suder. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
