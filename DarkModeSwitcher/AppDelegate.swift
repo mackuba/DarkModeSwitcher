8542314c3f26a1c38392b6d6c4d49ad14289c2c4
Kuba Suder <jakub.suder@gmail.com>
2020-08-16 00:00:01 +0200

    SwiftUI now works correctly with .fullSizeContentView

--- DarkModeSwitcher/AppDelegate.swift
+++ DarkModeSwitcher/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to initialize your application
         window = NSWindow(
             contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable],
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
             backing: .buffered, defer: false)
         window.center()
         window.setFrameAutosaveName("Main Window")
